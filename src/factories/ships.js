d41a7c4bdc841e5a6246c81e93d1c4016804ce09
Cililocwee <corrie.stroup@gmail.com>
2022-10-23 17:17:13 +0700

    initial commit

---
+++ src/factories/ships.js
@@ -0,0 +1,16 @@
+
+function sum(a,b) {
+    return a + b;
+}
+
+const ship = {
+    hit(){},
+    isSunk(){},
+    model: "battleship",
+    hp: 4,
+    orientation: 'vertical',
+    bow: [1,2]
+}
+
+
+export default sum;
---
+++ src/factories/tests/ships.test.js
@@ -0,0 +1,5 @@
+import sum from "../ships";
+
+test("1 + 3 should be 4", () => {
+    expect(sum(1,3)).toBe(4);
+})
