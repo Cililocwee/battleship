cb027eb24c62f3c0f9cbe2fcb315cb0606774b6a
Cililocwee <corrie.stroup@gmail.com>
2022-10-23 18:57:23 +0700

    added testing suite for ships

--- src/factories/ships.js
+++ src/factories/ships.js
@@ -1,16 +1,40 @@
+function Ship(model, orientation, bow) {
+  this.model = model;
+  this.orientation = orientation;
+  this.bow = bow;
+  this.status = "afloat";
 
-function sum(a,b) {
-    return a + b;
+  this.hit = function hit(strike = 1) {
+    this.hp -= strike;
+    if (this.hp <= 0) {
+      this.isSunk();
+    }
+  };
+  this.isSunk = function isSunk() {
+    this.status = "sunk";
+  };
+
+  switch (model) {
+    case "carrier":
+      this.hp = 5;
+      break;
+    case "battleship":
+      this.hp = 4;
+      break;
+    case "destroyer":
+      this.hp = 3;
+      break;
+    case "submarine":
+      this.hp = 3;
+      break;
+    case "patrolboat":
+      this.hp = 2;
+      break;
+  }
 }
 
-const ship = {
-    hit(){},
-    isSunk(){},
-    model: "battleship",
-    hp: 4,
-    orientation: 'vertical',
-    bow: [1,2]
+function shipYard(model, orientation, bow) {
+  return new Ship(model, orientation, bow);
 }
 
-
-export default sum;
+export default shipYard;
--- src/factories/tests/ships.test.js
+++ src/factories/tests/ships.test.js
@@ -1,5 +1,37 @@
-import sum from "../ships";
+import shipYard from "../ships";
 
-test("1 + 3 should be 4", () => {
-    expect(sum(1,3)).toBe(4);
-})
+test("Ship factory makes ships with correct attributes", () => {
+  const ship = shipYard("battleship", "vertical", [0, 0]);
+  expect(ship.model).toBe("battleship");
+  expect(ship.orientation).toBe("vertical");
+  expect(ship.bow).toEqual([0, 0]);
+  expect(ship.hp).toEqual(4);
+});
+
+test("Ship factory makes ships with correct attributes", () => {
+  const ship = shipYard("carrier", "vertical", [0, 0]);
+  expect(ship.model).toBe("carrier");
+  expect(ship.orientation).toBe("vertical");
+  expect(ship.bow).toEqual([0, 0]);
+  expect(ship.hp).toEqual(5);
+});
+
+test("Ship's hit function lowers hp", () => {
+  const ship = shipYard("battleship", "vertical", [0, 0]);
+  expect(ship.hp).toEqual(4);
+  ship.hit();
+  expect(ship.hp).toEqual(3);
+});
+
+test("Ship gets sunk when isSunk is called", () => {
+  const ship = shipYard("battleship", "vertical", [0, 0]);
+  expect(ship.status).toBe("afloat");
+  ship.isSunk();
+  expect(ship.status).toBe("sunk");
+});
+
+test("Ship gets sunk after enough hits", () => {
+  const ship = shipYard("battleship", "vertical", [0, 0]);
+  ship.hit(4);
+  expect(ship.status).toBe("sunk");
+});
