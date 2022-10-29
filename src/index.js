1f6b7274ab0e1980e7b4b0ccf50b407b29cc2787
Cililocwee <corrie.stroup@gmail.com>
2022-10-29 15:14:16 +0700

    displays grid labels correctly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import css from "./style.css";
 //  Create the games grids and layout
 
-let enemygrid = document.getElementById("enemygrid");
-let playergrid = document.getElementById("playergrid");
+const enemygrid = document.getElementById("enemygrid");
+const playergrid = document.getElementById("playergrid");
 
 // this may need to go on the gameboard object
 function displayGrid(target) {
@@ -16,6 +16,31 @@ function displayGrid(target) {
   }
 }
 
+const enemyLabelTop = document.getElementById("enemylabeltop");
+const enemyLabelLeft = document.getElementById("enemylabelleft");
+const playerLabelTop = document.getElementById("playerlabeltop");
+const playerLabelLeft = document.getElementById("playerlabelleft");
+
+// this may need to gon on the gameboard object
+function displayLabels(target, flag) {
+  for (let i = 0; i < 12; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    if (flag == 0) {
+      box.innerText = i;
+    }
+    if (flag == 1) {
+      box.innerText = String.fromCharCode(65 + i);
+    }
+
+    target.append(box);
+  }
+}
+
+displayLabels(enemyLabelTop, 0);
+displayLabels(enemyLabelLeft, 1);
+displayLabels(playerLabelTop, 0);
+displayLabels(playerLabelLeft, 1);
 function showCoords(event) {
   let x;
   let y;
